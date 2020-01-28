7580826dbe29c381ae7fb66605d6f53d5202c882
Kristján Oddsson <koddsson@gmail.com>
2020-01-28 12:47:44 +0000

    accept `text/fragment+html` as a content-type

--- src/send.js
+++ src/send.js
@@ -5,7 +5,7 @@ const requests = new WeakMap()
 export function fragment(el: Element, url: string): Promise<string> {
   const xhr = new XMLHttpRequest()
   xhr.open('GET', url, true)
-  xhr.setRequestHeader('Accept', 'text/html; fragment')
+  xhr.setRequestHeader('Accept', 'text/html; fragment, text/fragment+html')
   return request(el, xhr)
 }
 
