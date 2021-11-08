ff947151bcdf712318df26449fce9752d28cf778
Katie Foster <inkblotty@github.com>
2021-11-08 18:58:31 +0000

    set up screenreader logic from PR #51

---
+++ src/screen-reader-announcements.ts
@@ -0,0 +1,19 @@
+export const createAutoselectString = (firstOption: string): string => {
+  return `Press Enter to select ${firstOption}.`
+}
+
+export const createOptionsString = (numOptions: string): string => {
+  return `${numOptions} suggested options.`
+}
+
+export const createOptionsHiddenString = (): string => {
+  return `Suggestions hidden.`
+}
+
+export const createSelectionString = (selectionText: string): string => {
+  return `${selectionText} selected.`
+}
+
+export const createOptionsWithAutoselectString = (numOptions: string, firstOption: string): string => {
+  return `${createOptionsString(numOptions)} ${createAutoselectString(firstOption)}`
+}
